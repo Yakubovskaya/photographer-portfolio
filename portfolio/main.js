e300b716106ff83981d9ee85631f7badedfa49bd
Marina Yakubovskaya <maribars93@yandex.ru>
2025-10-04 17:48:09 +0300

    feat: add burger menu and accordion functional

---
+++ portfolio/main.js
@@ -0,0 +1,60 @@
+//burger menu
+const menu = document.querySelector(".menu__nav"),
+  menuItem = document.querySelectorAll(".nav__item"),
+  hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger--active");
+  menu.classList.toggle("menu__nav--active");
+  document.body.classList.toggle("modal-open");
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const targetID = item.getAttribute("href");
+
+    hamburger.classList.toggle("hamburger--active");
+    menu.classList.toggle("menu__nav--active");
+    document.body.classList.toggle("modal-open");
+
+    setTimeout(() => {
+      document.querySelector(targetID).scrollIntoView({
+        behavior: "smooth",
+      });
+    }, 300);
+  });
+});
+
+//accordion
+
+const accordionItems = document.querySelectorAll(".accordion__item"),
+  accordionHeader = document.querySelector(".accordion__label");
+
+const savedIndex = sessionStorage.getItem('activeAccordionIndex');
+if (savedIndex !== null && accordionItems[savedIndex]) {
+  accordionItems[savedIndex].classList.add('accordion__item--active');
+  accordionItems[0].classList.remove('accordion__item--active');
+} else {
+  accordionItems[0].classList.add('accordion__item--active');
+}
+
+accordionItems.forEach((item, index) => {
+  const header = item.querySelector('.accordion__label');
+  header.addEventListener("click", () => {
+    accordionItems.forEach((el) => {
+      if (el !== item) {
+        el.classList.remove("accordion__item--active")
+      }
+    });
+    item.classList.toggle("accordion__item--active");
+
+    if (item.classList.contains("accordion__item--active")) {
+      sessionStorage.setItem('activeAccordionIndex', index);
+    } else {
+      sessionStorage.removeItem('activeAccordionIndex');
+    }
+  });
+});
+
